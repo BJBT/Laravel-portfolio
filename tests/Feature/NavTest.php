117d02410feeceb96c11823d4b5f8461ec9c9cce
Blayne Bennett <blaynebennett99@gmail.com>
2021-01-19 11:42:27 -0700

    added ability to test in browser; WIP

---
+++ tests/Feature/NavTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class NavTest extends TestCase
+{
+
+    /** @test */
+    public function clicking_on_the_github_icon_sends_you_to_my_github()
+    {
+    }
+
+}
