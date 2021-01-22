e6448a7b70ef1e9e6a95526ed8bf697a11155788
Blayne Bennett <blaynebennett99@gmail.com>
2021-01-22 10:23:33 -0700

    attempt 2 on image diplay

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -437,8 +437,7 @@
                         <div class="w-full md:w-6/12 lg:w-3/12 lg:mb-0 mb-12 px-4">
                             <div class="px-6">
                                 <img alt="portfolio site"
-{{--                                     src="{{ asset('images/port_site.png') }}"--}}
-                                     src="{{ url('/images/port_site.png') }}"
+                                     src="{{ public_path('images/port_site.png') }}"
                                      class="h-48 w-full shadow-2xl rounded-lg mx-auto"
                                      style=""
                                 />
