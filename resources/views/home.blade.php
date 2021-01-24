87134148493f6d76643b7f7a3b1dfdfebb58c43b
Blayne Bennett <blaynebennett99@gmail.com>
2021-01-23 21:49:38 -0700

    displaying images for deployment

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -437,7 +437,7 @@
                         <div class="w-full md:w-6/12 lg:w-3/12 lg:mb-0 mb-12 px-4">
                             <div class="px-6">
                                 <img alt=""
-                                     src="{{ asset('/storage/images/port_site.png') }}"
+                                     src="{{ asset('storage/port_site.png') }}"
                                      class="h-48 w-full shadow-2xl rounded-lg mx-auto"
                                 />
                                 <div class="pt-6 text-center">
@@ -604,3 +604,7 @@
         }
     </script>
 </html>
+
+
+
+6191 2196 1801 2729
