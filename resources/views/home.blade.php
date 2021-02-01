819cc9148b8b9296b24a57b1634fc2e382470b87
Blayne Bennett <blaynebennett99@gmail.com>
2021-02-01 12:50:28 -0700

    edited contact information

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -544,74 +544,75 @@
                         </polygon>
                     </svg>
                 </div>
-                <div class="container mx-auto px-4 lg:pt-24 lg:pb-64">
+                <div class="container mx-auto px-4 lg:pt-24 lg:pb-34">
                     <div class="flex flex-wrap text-center justify-center">
                         <div class="w-full lg:w-6/12 px-4 mx-8 my-8">
                             <h2 class="text-4xl font-semibold text-white">Want to get in contact?</h2>
                             <p class="text-lg leading-relaxed mt-4 mb-4 text-gray-500">
-                                Complete this form and I will get back to you within 24 hours!
+                                Click my email below to shoot me a message and I will get back to you within 24 hours!
                             </p>
+                            <a class="font-bold text-2xl text-white hover:underline" href="mailto:blaynebennett99@gmail.com">blaynebennett99@gmail.com</a>
                         </div>
                     </div>
                 </div>
             </section>
-            <section class="relative block py-24 lg:pt-0 bg-gray-800">
-                <div class="container mx-auto px-4">
-                    <div class="flex flex-wrap justify-center lg:-mt-64 -mt-48">
-                        <div class="w-full lg:w-6/12 px-4">
-                            <div class="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-gray-300">
-                                <div class="flex-auto p-5 lg:p-10">
-                                    <div class="relative w-full mb-3 mt-8">
-                                        <label
-                                                class="block uppercase text-gray-700 text-xs font-bold mb-2"
-                                                for="full-name"
-                                        >Full Name</label
-                                        ><input
-                                                type="text"
-                                                class="px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full"
-                                                placeholder="Full Name"
-                                                style="transition: all 0.15s ease 0s;"
-                                        />
-                                    </div>
-                                    <div class="relative w-full mb-3">
-                                        <label
-                                                class="block uppercase text-gray-700 text-xs font-bold mb-2"
-                                                for="email"
-                                        >Email</label
-                                        ><input
-                                                type="email"
-                                                class="px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full"
-                                                placeholder="Email"
-                                                style="transition: all 0.15s ease 0s;"
-                                        />
-                                    </div>
-                                    <div class="relative w-full mb-3">
-                                        <label
-                                                class="block uppercase text-gray-700 text-xs font-bold mb-2"
-                                                for="message"
-                                        >Message</label
-                                        ><textarea
-                                                rows="4"
-                                                cols="80"
-                                                class="px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full"
-                                                placeholder="Type a message..."
-                                        ></textarea>
-                                    </div>
-                                    <div class="text-center mt-6">
-                                        <button
-                                                class="bg-gray-900 text-white active:bg-gray-700 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1"
-                                                type="button"
-                                                style="transition: all 0.15s ease 0s;"
-                                        >
-                                            Send Message
-                                        </button>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </section>
+{{--            <section class="relative block py-24 lg:pt-0 bg-gray-800">--}}
+{{--                <div class="container mx-auto px-4">--}}
+{{--                    <div class="flex flex-wrap justify-center lg:-mt-64 -mt-48">--}}
+{{--                        <div class="w-full lg:w-6/12 px-4">--}}
+{{--                            <div class="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-gray-300">--}}
+{{--                                <div class="flex-auto p-5 lg:p-10">--}}
+{{--                                    <div class="relative w-full mb-3 mt-8">--}}
+{{--                                        <label--}}
+{{--                                                class="block uppercase text-gray-700 text-xs font-bold mb-2"--}}
+{{--                                                for="full-name"--}}
+{{--                                        >Full Name</label--}}
+{{--                                        ><input--}}
+{{--                                                type="text"--}}
+{{--                                                class="px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full"--}}
+{{--                                                placeholder="Full Name"--}}
+{{--                                                style="transition: all 0.15s ease 0s;"--}}
+{{--                                        />--}}
+{{--                                    </div>--}}
+{{--                                    <div class="relative w-full mb-3">--}}
+{{--                                        <label--}}
+{{--                                                class="block uppercase text-gray-700 text-xs font-bold mb-2"--}}
+{{--                                                for="email"--}}
+{{--                                        >Email</label--}}
+{{--                                        ><input--}}
+{{--                                                type="email"--}}
+{{--                                                class="px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full"--}}
+{{--                                                placeholder="Email"--}}
+{{--                                                style="transition: all 0.15s ease 0s;"--}}
+{{--                                        />--}}
+{{--                                    </div>--}}
+{{--                                    <div class="relative w-full mb-3">--}}
+{{--                                        <label--}}
+{{--                                                class="block uppercase text-gray-700 text-xs font-bold mb-2"--}}
+{{--                                                for="message"--}}
+{{--                                        >Message</label--}}
+{{--                                        ><textarea--}}
+{{--                                                rows="4"--}}
+{{--                                                cols="80"--}}
+{{--                                                class="px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full"--}}
+{{--                                                placeholder="Type a message..."--}}
+{{--                                        ></textarea>--}}
+{{--                                    </div>--}}
+{{--                                    <div class="text-center mt-6">--}}
+{{--                                        <button--}}
+{{--                                                class="bg-gray-900 text-white active:bg-gray-700 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1"--}}
+{{--                                                type="button"--}}
+{{--                                                style="transition: all 0.15s ease 0s;"--}}
+{{--                                        >--}}
+{{--                                            Send Message--}}
+{{--                                        </button>--}}
+{{--                                    </div>--}}
+{{--                                </div>--}}
+{{--                            </div>--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--            </section>--}}
         </main>
     </body>
     <script>
